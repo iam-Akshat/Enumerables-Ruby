b750dc6abeacf1994bb92c7b63e158367cc7cd4c
Akshat Sethi <masterakshata@gmail.com>
2020-11-19 16:15:03 +0530

    created my_each_index

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -5,5 +5,12 @@ module Enumerable
           end
           self
         end
+    def my_each_with_index
+        index=0;
+        for item in self
+            yield(item,index)
+            index += 1
+        end
+        self    
+    end        
   end
-  
