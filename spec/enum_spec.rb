c6b765f9a3b37cb818e71712469d18e34b53b10f
Akshat Sethi <masterakshata@gmail.com>
2020-12-03 18:21:51 +0530

    tests for my_select

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -5,11 +5,32 @@ describe Enumerable do
 
   describe '#my_each' do
     it 'returns enum when no block given' do
-      expect(test1.my_each).to be_an Enumerable
+      expect(test1.my_each).to be_an Enumerator
     end
 
     it 'returns self when a block is given' do
       expect(test1.my_each { |i| }).to eql(test1)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'returns enum when no block given' do
+      expect(test1.my_each_with_index).to be_an Enumerator
+    end
+
+    it 'returns self when a block is given' do
+      expect(test1.my_each_with_index { |i| }).to eql(test1)
+    end
+  end
+
+  describe '#my_select' do
+    let(:all_num) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
+    it 'returns an enum when no block is given' do
+      expect(all_num.my_select).to be_an Enumerator
+    end
+
+    it 'returns an array which fullfills condition given in block' do
+      expect(all_num.my_select(&:even?)).to match_array [2, 4, 6, 8, 10]
     end
   end
 end
