6e43c126a78bf833b515744e9515d7044141f3ae
Akshat Sethi <masterakshata@gmail.com>
2020-12-03 19:17:32 +0530

    added tests for my_none?

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -57,4 +57,44 @@ describe Enumerable do
       expect(string_array.my_all? { |i| i.length > 5 }).to eql(false)
     end
   end
+
+  describe '#my_any?' do
+    let(:string_array) { %w[dog sheep bear] }
+
+    it 'if no block is given and array is empty returns false' do
+      expect([].my_any?).to eql(false)
+    end
+
+    it 'if no block is given returns true if any element is not false or nil.' do
+      expect([nil, 14, false].my_any?).to eql(true)
+    end
+
+    it 'if block is given returns true if any element satisfies block conditions' do
+      expect(string_array.my_any? { |i| i.length > 2 }).to eql(true)
+    end
+
+    it 'if block is given returns true if any element satisfies block conditions' do
+      expect(string_array.my_any? { |i| i.length > 4 }).to eql(true)
+    end
+  end
+
+  describe '#my_none?' do
+    let(:string_array) { %w[dog sheep bear] }
+
+    it 'returns true if no block given and none of the elements is true ' do
+      expect([nil, false].my_none?).to eql(true)
+    end
+
+    it 'returns false if no block given and any of the elements is not false' do
+      expect([nil, false, 10].my_none?).to eql(false)
+    end
+
+    it 'returns true when none of the element satisfies block conditions' do
+      expect(string_array.my_none? { |word| word.length == 6 }).to eql(true)
+    end
+
+    it 'returns false when any of the element satisfies block conditions' do
+      expect(string_array.my_none? { |word| word.length == 5 }).to eql(false)
+    end
+  end
 end
